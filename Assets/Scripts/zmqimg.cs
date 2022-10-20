7154257af6b1ecac9bd51d509b66c9625f214c6f
PinkPanther-ny <alvincny529@gmail.com>
2022-10-21 02:20:56 +0800

    Add screen

---
+++ Assets/Scripts/zmqimg.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System;
+using NetMQ;
+using NetMQ.Sockets;
+using Debug = UnityEngine.Debug;
+using System.IO;
+using System.Drawing;
+
+public class zmqimg : MonoBehaviour
+{
+    Texture2D t;
+    SubscriberSocket subSocket;
+    private void Awake()
+    {
+        AsyncIO.ForceDotNet.Force();
+        subSocket = new SubscriberSocket();
+        subSocket.Options.ReceiveHighWatermark = 1000;
+        subSocket.Connect("tcp://10.42.0.1:8000");
+        subSocket.Subscribe("");
+    }
+
+    void Update()
+    {
+
+        string messageReceived = subSocket.ReceiveFrameString();
+        Debug.Log(messageReceived.Length);
+        byte[] bytebuffer = Convert.FromBase64String(messageReceived);
+
+        Texture2D tex = new Texture2D(1280, 480);
+        tex.LoadImage(bytebuffer);
+        tex.Apply();
+        GetComponent<Renderer>().material.mainTexture = tex;
+
+    }
+}
